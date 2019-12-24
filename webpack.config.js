800c1e9375f249417507924a92911e3bfbf64060
filkovskyi <filkovskyi@gmail.com>
2019-12-24 15:29:20 +0200

    refactor(topick_picker): add topick_picker

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
 // webpack.config.js
-const path = require("path");
+const path = require("path")
 
-var webpack = require("webpack");
-var package = require("./package.json");
+const webpack = require("webpack")
+const package = require("./package.json")
 
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: ["./src/index.js", "./src/auto-complete.js", "./src/index.css"],
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: `${package.name}-${package.version}.bundle.js`
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: "script-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] }
-    ]
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    }),
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "LS Search Bar",
-      inject: "body",
-      template: "src/index.html"
-    })
-  ],
-  devtool: "source-map",
-  devServer: {
-    writeToDisk: true,
-    compress: true,
-    port: 3002,
-    hot: true
-  }
-};
+    entry: ["./src/index.js", "./src/auto-complete.js", "./src/index.css"],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: `${package.name}-${package.version}.bundle.js`
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: "script-loader" },
+            { test: /\.css$/, use: ["style-loader", "css-loader"] }
+        ]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify("production")
+        }),
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "LS Search Bar",
+            inject: "body",
+            template: "src/index.html"
+        })
+    ],
+    devtool: "source-map",
+    devServer: {
+        writeToDisk: true,
+        compress: true,
+        port: 3001,
+        hot: true
+    }
+}
